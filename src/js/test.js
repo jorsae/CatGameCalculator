401bc7fa6a7d6732d8f1de06ee0452ba2efcef46
jorsae <jorgen.sater@gmail.com>
2020-08-09 09:17:35 +1000

    Changed test.js to utility new functions made as a test

--- src/js/test.js
+++ src/js/test.js
@@ -1,9 +1,23 @@
 import { Floor } from './classes/floor';
 import { getCraftingRecipes } from '../js/normal/recipes';
+import { Crafting } from './classes/crafting';
 
 const mayhem = new Floor("Mayhem", 4, null);
 console.log("hello world");
 console.log(mayhem.name);
 
 const recipes = getCraftingRecipes();
-const item = recipes.get("Needles");
+var crafting = new Crafting(recipes);
+crafting.addCraftingItem('Needles', 3);
+crafting.addCraftingItem('Water', 3);
+crafting.addCraftingItem('Fire', 6);
+crafting.addCraftingItem('Pendant', 2);
+crafting.addCraftingItem('Elementstone', 1);
+
+var reqs = crafting.getCraftingRequirements();
+console.log('Requirements: ');
+for (const [name, quantity] of reqs.entries()) {
+    console.log(name + ': ' + quantity);
+}
+var cost = crafting.getTotalCost();
+console.log('cost: ' + cost);
