c7b2c44a7e39e0f300e96288f5ff81c300a5a086
jorsae <jorgen.sater@gmail.com>
2020-08-09 09:14:05 +1000

    Added class Crafting and moved getCraftingRequirements with tests there

--- src/js/classes/crafting.js
+++ src/js/classes/crafting.js
@@ -1,7 +1,9 @@
 export class Crafting{
-    constructor(){
+    constructor(craftingRecipes){
+        this.craftingRecipes = craftingRecipes;
         this.craftingList = new Map();
         this.craftingTime = 30;
+        this.currentCraft = new Map();
     }
 
     addCraftingItem(item){
@@ -17,5 +19,52 @@ export class Crafting{
 
     setCraftingTime(time){
         this.craftingTime = time;
+    }
+
+    getCraftingRequirements(){
+        this.currentCraft.clear();
+
+        for (const [name, craftingItem] of this.craftingList.entries()) {
+            this.getItemRequirements(craftingItem, craftingItem.quantity);
+            this.addElementToCurrentCraft(craftingItem.name, craftingItem.quantity);
+
+        }
+        return this.currentCraft;
+    }
+
+    /**
+     * Recursive function that will fetch all crafting requirements from a given item.
+     * TODO: It should not be needed to have currentCraft as a parameter.
+     */
+    getItemRequirements(item, parentQuantity = 1){
+        if(item.craftingRequirements === null){
+            return;
+        }
+
+        // Adds all the requirements
+        for(var i = 0; i < item.craftingRequirements.length; i++){
+            var name = item.craftingRequirements[i].craftingItem.name;
+            var quantity = item.craftingRequirements[i].quantity * parentQuantity;
+            
+            var craftingItem = this.craftingRecipes.get(name);
+            this.getItemRequirements(craftingItem, quantity);
+            this.addElementToCurrentCraft(name, quantity);
+        }
+        return this.currentCraft;
+    }
+        
+    /**
+     * Adds key and value to a map.
+     * If the key already exists, increase the value
+     */
+    addElementToCurrentCraft(key, value){
+        var name = this.currentCraft.get(key);
+        if(name === undefined){
+            this.currentCraft.set(key, value);
+        }
+        else{
+            var oldValue = this.currentCraft.get(key);
+            this.currentCraft.set(key, oldValue + value);
+        }
     }
 }
--- test/classes/craftingTest.js
+++ test/classes/craftingTest.js
@@ -1,15 +1,16 @@
 var assert = require('assert');
 var craftingItem = require('../../src/js/classes/craftingItem');
 var crafting = require('../../src/js/classes/crafting');
+var recipe = require('../../src/js/normal/recipes');
 
 describe('Test for Crafting', () => {
     it('Crafting constructor', () => {
-        var c = new crafting.Crafting();
+        var c = new crafting.Crafting(null);
         assert.equal(c.craftingTime, 30);
     });
 
     it('Crafting change craftingTime', () => {
-        var c = new crafting.Crafting();
+        var c = new crafting.Crafting(null);
         assert.equal(c.craftingTime, 30);
 
         c.setCraftingTime(160);
@@ -18,7 +19,7 @@ describe('Test for Crafting', () => {
 
     it('Crafting addCraftingItem one item', () => {
         const item = new craftingItem.CraftingItem("craftingItem", 50, 100, "rare", 0);
-        var c = new crafting.Crafting();
+        var c = new crafting.Crafting(null);
         c.addCraftingItem(item);
         
         var expected = new Map();
@@ -30,7 +31,7 @@ describe('Test for Crafting', () => {
         const item = new craftingItem.CraftingItem("craftingItem", 50, 100, "rare", 0);
         const item2 = new craftingItem.CraftingItem("craftingItem2", 50, 100, "rare", 0);
 
-        var c = new crafting.Crafting();
+        var c = new crafting.Crafting(null);
         c.addCraftingItem(item);
         c.addCraftingItem(item2);
 
@@ -46,7 +47,7 @@ describe('Test for Crafting', () => {
         const item2 = new craftingItem.CraftingItem("craftingItem", 50, 100, "rare", 0, null, 7);
         const item3 = new craftingItem.CraftingItem("craftingItem", 50, 100, "rare", 0, null, 10);
 
-        var c = new crafting.Crafting();
+        var c = new crafting.Crafting(null);
         c.addCraftingItem(item);
         c.addCraftingItem(item2);
         
@@ -60,5 +61,78 @@ describe('Test for Crafting', () => {
         expected2.set(expectedItem2.name, expectedItem2);
         c.addCraftingItem(item3);
         assert.deepEqual(c.craftingList, expected2);
+    });
+
+    it('Crafting getCraftingRequirements: 2 needles', () => {
+        var recipes = recipe.getCraftingRecipes();
+        var needles = recipes.get('Needles');
+        needles.quantity = 2;
+
+        var c = new crafting.Crafting(recipes);
+        c.addCraftingItem(needles);
+        
+        var currentCraft = c.getCraftingRequirements();
+        var expected = new Map();
+        expected.set('Cotton', 12);
+        expected.set('Log', 12);
+        expected.set('Rock', 24);
+        expected.set('String', 4);
+        expected.set('Wood', 4);
+        expected.set('Ribbon', 2);
+        expected.set('Metal', 8);
+        expected.set('Needles', 2);
+
+        assert.deepEqual(currentCraft, expected);
+    });
+
+    it('Crafting getCraftingRequirements. Non over-lapping crafting items', () => {
+        var recipes = recipe.getCraftingRecipes();
+        var bronze = recipes.get('Bronze');
+        var ribbon = recipes.get('Ribbon');
+
+        var c = new crafting.Crafting(recipes);
+        c.addCraftingItem(bronze);
+        c.addCraftingItem(ribbon);
+        
+        var currentCraft = c.getCraftingRequirements();
+        var expected = new Map();
+        expected.set('Cotton', 6);
+        expected.set('Log', 6);
+        expected.set('Rock', 4);
+        expected.set('String', 2);
+        expected.set('Wood', 2);
+        expected.set('Ribbon', 1);
+        expected.set('Bronze', 1);
+
+        assert.deepEqual(currentCraft, expected);
+    });
+
+    it('Crafting getCraftingRequirements. Over-lapping crafting items', () => {
+        var recipes = recipe.getCraftingRecipes();
+        var amethyst = recipes.get('Amethyst');
+        var pendant = recipes.get('Pendant');
+
+        var c = new crafting.Crafting(recipes);
+        c.addCraftingItem(amethyst);
+        c.addCraftingItem(pendant);
+        
+        var currentCraft = c.getCraftingRequirements();
+        var expected = new Map();
+        expected.set('Cotton', 48);
+        expected.set('Log', 48);
+        expected.set('Rock', 72);
+        expected.set('Quartz', 80);
+        expected.set('String', 16);
+        expected.set('Wood', 16);
+        expected.set('Ribbon', 8);
+        expected.set('Metal', 16);
+        expected.set('Needles', 4);
+        expected.set('Sparkles', 2);
+        expected.set('Bronze', 6);
+        expected.set('Silver', 2);
+        expected.set('Amethyst', 8);
+        expected.set('Pendant', 1);
+
+        assert.deepEqual(currentCraft, expected);
     });
 });
