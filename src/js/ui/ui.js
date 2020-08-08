19757f5cbe8c5bf650aeef99aa6677ac4c45289e
jorsae <jorgen.sater@gmail.com>
2020-08-09 09:30:43 +1000

    Added copy to clipboard and displaying 'how to'

--- src/js/ui/ui.js
+++ src/js/ui/ui.js
@@ -14,9 +14,78 @@ export function initialize(rec, floorRec){
     document.getElementById("quickCalculate").onclick = calculate;
     document.getElementById("addFloor").onclick = addFloor;
     document.getElementById("clear").onclick = clear;
+    document.getElementById("copyClipboard").onclick = copyClipboard;
+    document.getElementById("howToUse").onclick = displayHowTo;
 
     populateCraftingItems(recipes);
     populateFloors(floorRecipes);
+}
+
+/**
+ * Function that sets up click events to display/hide How-to guide
+ */
+function displayHowTo(){
+    const guideContainer = document.getElementById("guideContainer");
+    if(guideContainer.classList.contains("guide-container-height")){
+        guideContainer.classList.remove("guide-container-height");
+    }
+    else{
+        guideContainer.classList.add("guide-container-height");
+    }
+}
+
+/**
+ * Puts the output table to the users clipboard.
+ * We can only copy to clipboard things that are on the website itself.
+ * We create a invisible div and create a clone of the table, without the first column.
+ * We then copy that table and then remove the div so everything is deleted afterwards.
+ */
+function copyClipboard(){
+    var table = document.createElement("table");
+
+    // Add table header
+    var tableHeader = document.querySelectorAll("#outputTable tr th");
+    var thead = document.createElement("thead");
+    table.appendChild(thead);
+
+    var tableRow = document.createElement("tr");
+    thead.appendChild(tableRow);
+    for(var i = 0; i < tableHeader.length; i++){
+        var clone = tableHeader[i].cloneNode(true);
+        tableRow.appendChild(clone);
+    }
+    thead.appendChild(tableRow);
+
+    // Add table data
+    var tableData = document.querySelectorAll("#outputTable tr td");
+    var tbody = document.createElement("tbody");
+    var tableRow = document.createElement("tr");
+    for(var i = 0; i < tableData.length; i++){
+        var clone = tableData[i].cloneNode(true);
+        tableRow.appendChild(tableData[i].cloneNode(true));
+        if((i % 4) === 3 && i !== 0){
+            tbody.appendChild(tableRow);
+            tableRow = document.createElement("tr");
+        }
+    }
+    table.appendChild(tbody);
+
+    // "Invisible" div to store the table we created
+    var tableDiv = document.createElement("div");
+    tableDiv.style = "position: absolute; top: -9999999px; opacity: 0;";
+    tableDiv.appendChild(table);
+    document.body.appendChild(tableDiv);
+
+    // Puts the table in the users clipboard
+    var range = document.createRange();  
+    range.selectNode(table);
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+    document.execCommand('copy');
+    
+    document.body.removeChild(tableDiv);
+
+    displayPopover('copyClipboard', 'Copied!');
 }
 
 function clear(){
