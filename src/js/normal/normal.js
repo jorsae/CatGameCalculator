ec3a24b8afe0ccb92851afee131e4ec1a9bbc168
jorsae <jorgen.sater@gmail.com>
2020-08-09 09:22:38 +1000

    Will now generate html for all crafting-items

--- src/js/normal/normal.js
+++ src/js/normal/normal.js
@@ -1,16 +1,17 @@
 import { getCraftingRecipes } from './recipes';
 import { Crafting } from '../classes/crafting';
-import { createCraftingItem } from '../ui/ui';
+import { populateCraftingItems } from '../ui/ui';
 
 // init function
 $(document).ready(function(){
     const recipes = getCraftingRecipes();
     var crafting = new Crafting(recipes);
 
-    var test = document.getElementById("test");
+    populateCraftingItems(recipes);
+    /*var test = document.getElementById("test");
     for (const [name, item] of recipes.entries()) {
         if(name === 'Ribbon'){
             createCraftingItem(test, item);
         }
-    }
+    }*/
 });
--- src/js/ui/ui.js
+++ src/js/ui/ui.js
@@ -1,22 +1,63 @@
-export function populateCraftingItems(parentElement, recipes){
-    // TODO: This should call createCraftingItem for each element in the recipes that is not RAW or HIDDEN rarity.
-    // it should also create the rest of the elements needed, fr example for each caategory.
+export function populateCraftingItems(recipes){
+    var craftingDiv = document.getElementById("craftingContainer");
+
+    var lastCategory = null;
+    var currentTier = null;
+    for (const [name, item] of recipes.entries()) {
+        if(item.rarity == 'raw' || item.rarity == 'hidden'){
+            continue;
+        }
+
+        if(item.category !== lastCategory){
+            lastCategory = item.category;
+            
+            // Create and append header to #craftingContainer
+            let header = createCraftingTierHeader(item.category);
+            craftingDiv.appendChild(header);
+            
+            var tier = createCraftingTier();
+            craftingDiv.appendChild(tier);
+            currentTier = tier;
+
+            createCraftingItem(currentTier, item);
+        }
+        else{
+            createCraftingItem(currentTier, item);
+        }
+    }
+}
+
+function createCraftingTier(){
+    var tier = document.createElement("div");
+    tier.classList.add("crafting-tier");
+    return tier;
+}
+
+function createCraftingTierHeader(category){
+    var header = document.createElement("h2");
+    var textHeader = document.createTextNode(category);
+    header.appendChild(textHeader);
+    return header;
 }
 
 // This function creates a crafting-item with all DOM elements needed
-export function createCraftingItem(element, item){
+function createCraftingItem(tier, item){
+    var itemDiv = document.createElement("div");
+    itemDiv.classList.add("crafting-item");
+    itemDiv.classList.add(item.rarity);
+
     // Creates button which act as a header and as a dropdown button for more info about the item
     const craftingItemButton = document.createElement("button"); // Button elements for the crafting items
     craftingItemButton.classList.add("crafting-item-button");
     craftingItemButton.innerHTML = item.name + ' <i class="fa fa-angle-double-down"></i>';
-    element.appendChild(craftingItemButton);
+    itemDiv.appendChild(craftingItemButton);
 
     // Creates the p tag that holds item information
     //<p class="crafting-item-info" id="woodInfo"></p>
     const craftingItemInfo = document.createElement("p");
     craftingItemInfo.classList.add("crafting-item-info");
     craftingItemInfo.id = item.name + "Info";
-    element.appendChild(craftingItemInfo);
+    itemDiv.appendChild(craftingItemInfo);
 
     // Input field for how many to craft
     // <input id="WoodAmount" class="crafting-item-amount" aria-label="Amount of wood you want to craft"
@@ -28,7 +69,7 @@ export function createCraftingItem(element, item){
     craftingAmount.value = 0;
     craftingAmount.min = 0;
     craftingAmount.addEventListener("click", function() { this.select(); });
-    element.appendChild(craftingAmount);
+    itemDiv.appendChild(craftingAmount);
 
     const incrementContainer = document.createElement("span");
 
@@ -42,7 +83,9 @@ export function createCraftingItem(element, item){
     downArrow.addEventListener("click", function() { downClick(item.name); } );
     incrementContainer.appendChild(downArrow);
     
-    element.appendChild(incrementContainer);
+    itemDiv.appendChild(incrementContainer);
+
+    tier.appendChild(itemDiv);
 }
 
 function upClick(name){
