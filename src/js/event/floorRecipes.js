b2631f759bc7caf695f9080c44cf01c9f31b9621
jorsae <jorgen.sater@gmail.com>
2020-08-12 12:14:00 +1000

    Removed eventNames and hard coded in generic names

--- src/js/event/floorRecipes.js
+++ src/js/event/floorRecipes.js
@@ -1,83 +1,82 @@
 import { Floor } from '../classes/floor';
-import { craftingItemNames, prizeNames } from './eventNames';
 
 export function getFloorRecipes(){
     var floorRecipes = new Map();
 
-    const prizeAllReq = [[craftingItemNames[0], 5],
-                        [craftingItemNames[1], 8],
-                        [craftingItemNames[2], 7],
-                        [craftingItemNames[3], 32],
-                        [craftingItemNames[4], 21],
-                        [craftingItemNames[5], 11],
+    const prizeAllReq = [["Item A", 5],
+                        ["Item B", 8],
+                        ["Item C", 7],
+                        ["Item D", 32],
+                        ["Item E", 21],
+                        ["Item F", 11],
                     ];
     const prizeAll = new Floor("All decorations/cats", 0, prizeAllReq);
     floorRecipes.set(prizeAll.name, prizeAll);
                     
-    const prize1Req = [[craftingItemNames[2], 3],
-                        [craftingItemNames[3], 2],
+    const prize1Req = [["Item C", 3],
+                        ["Item D", 2],
                     ];
-    const prize1 = new Floor(prizeNames[0], 1, prize1Req);
+    const prize1 = new Floor("Cat 1", 1, prize1Req);
     floorRecipes.set(prize1.name, prize1);
     
-    const prize2Req = [ [craftingItemNames[3], 1] ]
-    const prize2 = new Floor(prizeNames[1], 2, prize2Req);
+    const prize2Req = [ ["Item D", 1] ]
+    const prize2 = new Floor("Decoration 1", 2, prize2Req);
     floorRecipes.set(prize2.name, prize2);
     
-    const prize3Req = [[craftingItemNames[0], 5],
-                        [craftingItemNames[3], 3],
-                        [craftingItemNames[4], 2],
+    const prize3Req = [["Item A", 5],
+                        ["Item D", 3],
+                        ["Item E", 2],
                     ];
-    const prize3 = new Floor(prizeNames[2], 3, prize3Req);
+    const prize3 = new Floor("Cat 2", 3, prize3Req);
     floorRecipes.set(prize3.name, prize3);
     
-    const prize4Req = [[craftingItemNames[1], 2],
-                        [craftingItemNames[3], 2],
-                        [craftingItemNames[4], 1],
+    const prize4Req = [["Item B", 2],
+                        ["Item D", 2],
+                        ["Item E", 1],
                     ];
-    const prize4 = new Floor(prizeNames[3], 4, prize4Req);
+    const prize4 = new Floor("Decoration 2", 4, prize4Req);
     floorRecipes.set(prize4.name, prize4);
     
-    const prize5Req = [[craftingItemNames[1], 6],
-                        [craftingItemNames[3], 6],
-                        [craftingItemNames[4], 4],
+    const prize5Req = [["Item B", 6],
+                        ["Item D", 6],
+                        ["Item E", 4],
                     ];
-    const prize5 = new Floor(prizeNames[4], 5, prize5Req);
+    const prize5 = new Floor("Cat 3", 5, prize5Req);
     floorRecipes.set(prize5.name, prize5);
     
-    const prize6Req = [[craftingItemNames[2], 4],
-                        [craftingItemNames[3], 4],
-                        [craftingItemNames[4], 3],
+    const prize6Req = [["Item C", 4],
+                        ["Item D", 4],
+                        ["Item E", 3],
                     ];
-    const prize6 = new Floor(prizeNames[5], 6, prize6Req);
+    const prize6 = new Floor("Decoration 3", 6, prize6Req);
     floorRecipes.set(prize6.name, prize6);
     
-    const prize7Req = [[craftingItemNames[3], 4],
-                        [craftingItemNames[4], 3],
-                        [craftingItemNames[5], 2],
+    const prize7Req = [["Item D", 4],
+                        ["Item E", 3],
+                        ["Item F", 2],
                     ];
-    const prize7 = new Floor(prizeNames[6], 7, prize7Req);
+    const prize7 = new Floor("Cat 4", 7, prize7Req);
     floorRecipes.set(prize7.name, prize7);
     
-    const prize8Req = [[craftingItemNames[3], 1],
-                        [craftingItemNames[4], 2],
-                        [craftingItemNames[5], 2],
+    const prize8Req = [["Item D", 1],
+                        ["Item E", 2],
+                        ["Item F", 2],
                     ];
-    const prize8 = new Floor(prizeNames[7], 8, prize8Req);
+    const prize8 = new Floor("Decoration 4", 8, prize8Req);
     floorRecipes.set(prize8.name, prize8);
     
-    const prize9Req = [[craftingItemNames[3], 4],
-                        [craftingItemNames[4], 3],
-                        [craftingItemNames[5], 3],
+    const prize9Req = [["Item D", 4],
+                        ["Item E", 3],
+                        ["Item F", 3],
                     ];
-    const prize9 = new Floor(prizeNames[8], 9, prize9Req);
+    const prize9 = new Floor("Decoration 5", 9, prize9Req);
     floorRecipes.set(prize9.name, prize9);
     
-    const prize10Req = [[craftingItemNames[3], 5],
-                        [craftingItemNames[4], 3],
-                        [craftingItemNames[5], 4],
+    const prize10Req = [["Item D", 5],
+                        ["Item E", 3],
+                        ["Item F", 4],
                     ];
-    const prize10 = new Floor(prizeNames[9], 10, prize10Req);
+    const prize10 = new Floor("Decoration 6", 10, prize10Req);
     floorRecipes.set(prize10.name, prize10);
 
     return floorRecipes;
--- src/js/event/recipes.js
+++ src/js/event/recipes.js
@@ -1,7 +1,6 @@
 var craftingItem = require('../classes/craftingItem');
 var craftingRequirement = require('../classes/craftingRequirement');
 var rarity = require('../classes/rarity');
-var eventNames = require('./eventNames');
 
 /**
  * We wrap adding the crafting recipes in a function so we can call it from window.onload.
@@ -11,43 +10,43 @@ export function getCraftingRecipes(){
     var craftingRecipes = new Map();
     
     // Base
-    const rawMaterialA = new craftingItem.CraftingItem(eventNames.rawMaterialNames[0], eventNames.categories[0], 0, 0, rarity.Rarity.RAW, 1);
-    const rawMaterialB = new craftingItem.CraftingItem(eventNames.rawMaterialNames[1], eventNames.categories[0], 0, 0, rarity.Rarity.RAW, 2);
-    const rawMaterialC = new craftingItem.CraftingItem(eventNames.rawMaterialNames[2], eventNames.categories[0], 0, 0, rarity.Rarity.RAW, 3);
+    const rawMaterialA = new craftingItem.CraftingItem("Raw Material A", "", 0, 0, rarity.Rarity.RAW, 1);
+    const rawMaterialB = new craftingItem.CraftingItem("Raw Material B", "", 0, 0, rarity.Rarity.RAW, 2);
+    const rawMaterialC = new craftingItem.CraftingItem("Raw Material C", "", 0, 0, rarity.Rarity.RAW, 3);
     craftingRecipes.set(rawMaterialA.name, rawMaterialA);
     craftingRecipes.set(rawMaterialB.name, rawMaterialB);
     craftingRecipes.set(rawMaterialC.name, rawMaterialC);
     
     // Tier 1
     const AReq = [new craftingRequirement.CraftingRequirement(rawMaterialA, 4)];
-    const A = new craftingItem.CraftingItem(eventNames.craftingItemNames[0], eventNames.categories[1], 20, 15, rarity.Rarity.COMMON, 5, AReq);
+    const A = new craftingItem.CraftingItem("Item A", "Tier 1 Crafting", 20, 15, rarity.Rarity.COMMON, 5, AReq);
     craftingRecipes.set(A.name, A);
     
     const BReq = [new craftingRequirement.CraftingRequirement(rawMaterialB, 4)];
-    const B = new craftingItem.CraftingItem(eventNames.craftingItemNames[1], eventNames.categories[1], 20, 15, rarity.Rarity.COMMON, 6, BReq);
+    const B = new craftingItem.CraftingItem("Item B", "Tier 1 Crafting", 20, 15, rarity.Rarity.COMMON, 6, BReq);
     craftingRecipes.set(B.name, B);
     
     const CReq = [new craftingRequirement.CraftingRequirement(rawMaterialC, 4)];
-    const C = new craftingItem.CraftingItem(eventNames.craftingItemNames[2], eventNames.categories[1], 20, 15, rarity.Rarity.COMMON, 7, CReq);
+    const C = new craftingItem.CraftingItem("Item C", "Tier 1 Crafting", 20, 15, rarity.Rarity.COMMON, 7, CReq);
     craftingRecipes.set(C.name, C);
     
     // Tier 2
     const DReq = [new craftingRequirement.CraftingRequirement(A, 3), new craftingRequirement.CraftingRequirement(B, 2), new craftingRequirement.CraftingRequirement(C, 2)];
-    const D = new craftingItem.CraftingItem(eventNames.craftingItemNames[3], eventNames.categories[2], 150, 15, rarity.Rarity.RARE, 8, DReq);
+    const D = new craftingItem.CraftingItem("Item D", "Tier 2 Crafting", 150, 15, rarity.Rarity.RARE, 8, DReq);
     craftingRecipes.set(D.name, D);
     
     const EReq = [new craftingRequirement.CraftingRequirement(D, 2), new craftingRequirement.CraftingRequirement(C, 4), new craftingRequirement.CraftingRequirement(B, 4)];
-    const E = new craftingItem.CraftingItem(eventNames.craftingItemNames[4], eventNames.categories[2], 200, 30, rarity.Rarity.RARE, 9, EReq);
+    const E = new craftingItem.CraftingItem("Item E", "Tier 2 Crafting", 200, 30, rarity.Rarity.RARE, 9, EReq);
     craftingRecipes.set(E.name, E);
     
     const FReq = [new craftingRequirement.CraftingRequirement(E, 3), new craftingRequirement.CraftingRequirement(A, 8)];
-    const F = new craftingItem.CraftingItem(eventNames.craftingItemNames[5], eventNames.categories[2], 400, 60, rarity.Rarity.RARE, 10, FReq);
+    const F = new craftingItem.CraftingItem("Item F", "Tier 2 Crafting", 400, 60, rarity.Rarity.RARE, 10, FReq);
     craftingRecipes.set(F.name, F);
 
     // Tier 3
     const starsReq = [new craftingRequirement.CraftingRequirement(A, 1), new craftingRequirement.CraftingRequirement(B, 1),
                     new craftingRequirement.CraftingRequirement(C, 1), new craftingRequirement.CraftingRequirement(E, 1)];
-    const stars = new craftingItem.CraftingItem(eventNames.craftingItemNames[6], eventNames.categories[3], 300, 0, rarity.Rarity.RARE, 11, starsReq);
+    const stars = new craftingItem.CraftingItem("3 Stars", "Star Crafting", 300, 0, rarity.Rarity.RARE, 11, starsReq);
     craftingRecipes.set(stars.name, stars);
 
     return craftingRecipes;
