04468f9a66cd3a836ff794333f798d3e05dfd12b
jorsae <jorgen.sater@gmail.com>
2020-09-12 19:08:45 +1000

    Set cookies to SameSite=strict

--- src/js/utility/cookie.js
+++ src/js/utility/cookie.js
@@ -8,7 +8,7 @@ export function setCookie(name, value, days) {
         date.setTime(date.getTime() + (days*24*60*60*1000));
         expires = "; expires=" + date.toUTCString();
     }
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/;SameSite=strict";
 }
 
 /**
