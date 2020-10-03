e962ce96424cbe7f90e27a9982b5e07f8d18ad57
jorsae <jorgen.sater@gmail.com>
2020-10-03 11:21:50 +1000

    Made simple functions for progress.html

--- src/js/progress/progress.js
+++ src/js/progress/progress.js
@@ -1,5 +1,37 @@
-var key = findGetParameter('key'); // normal or event
-console.log(key);
+import { jsonToMap } from '../utility/utility';
+
+var key = "normal";
+
+// init function
+$(document).ready(function(){
+    document.getElementById("tt").onclick = test;
+
+    // TODO: This is not gonna be used untill everything is done
+    // normal.html and event.html, should have this in the GET request to auto-fill the form for the user
+    var key = findGetParameter('key'); // normal or event
+    console.log(key);
+});
+
+function test(){
+    var craftingType = document.getElementById("craftingType").value;
+    console.log(craftingType);
+
+    var craftingItems = loadCraftingItems(craftingType + "Crafting");
+    console.log("craftingItems: ");
+    console.log(craftingItems);
+
+    var inventoryItems = loadCraftingItems(craftingType + "Inventory");
+    console.log("Inventory: ");
+    console.log(inventoryItems);
+}
+
+function loadCraftingItems(craftingKey){
+    var savedCrafting = localStorage.getItem(craftingKey);
+    if(savedCrafting === null){
+        return new Map(); // No saved inventory
+    }
+    return jsonToMap(savedCrafting);
+}
 
 function findGetParameter(parameterName) {
     var result = null,
